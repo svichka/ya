9bf2c5e2dde1df70ec6d2ae18fa908b83f6917cf
WreWolf <wrewolf@gmail.com>
2017-09-29 15:51:47 +0300

    Пресоздал проект к хуям, по какой то причине перестал работать старый...

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,12 +4,10 @@
   
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-  use Symfony\Component\HttpFoundation\JsonResponse;
   use Symfony\Component\HttpFoundation\Request;
   
   class SecurityController extends Base
   {
-  
     /**
      * @Route("/login/", name="login")
      */
@@ -18,7 +16,7 @@
       $authenticationUtils = $this->get('security.authentication_utils');
       $error = $authenticationUtils->getLastAuthenticationError();
       $lastUsername = $authenticationUtils->getLastUsername();
-    
+      
       $user = $this->getUser();
       if ($user) {
         $userRoles = $user->getRoles();
@@ -34,79 +32,11 @@
           $this->container->get('security.context')->setToken(null);
         }
       }
-    
+      
       return $this->render('AppBundle:Default:login.html.twig', array(
         'last_username' => $lastUsername,
         'error' => $error,
       ));
-    }
-  
-  
-  
-    /**
-     * @Route("/login_json/", name="login_json")
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     *
-     * @return JsonResponse
-     */
-    public function loginJsonAction(Request $request)
-    {
-      
-      $authenticationUtils = $this->get('security.authentication_utils');
-      $error = $authenticationUtils->getLastAuthenticationError();
-      $lastUsername = $authenticationUtils->getLastUsername();
-      $log = $this->get('logger');
-      
-      $user = $this->getUser();
-      if ($user)
-      {
-        $log->error("user");
-        $userRoles = $user->getRoles();
-        
-        if (in_array('ROLE_USER', $userRoles))
-        {
-          return JsonResponse::create(
-            [
-              'status'        => 200,
-              'last_username' => $lastUsername,
-              'error'         => $error,
-            ]);
-        }
-        elseif (in_array('ROLE_NOT_ACTIVE_USER', $userRoles))
-        {
-          if (in_array('ROLE_NOT_ACTIVE_USER_NOT_ACTIVE_MOBILE', $userRoles))
-          {
-            $error = ['messageKey' => "Телефон не активирован", 'messageData' => []];
-            if ($user->getParticipant() !== null)
-            {
-              return JsonResponse::create(
-                [
-                  'status'        => 200,
-                  'last_username' => $lastUsername,
-                  'error'         => $error,
-                ]);
-            }
-          }
-          if (in_array('ROLE_NOT_ACTIVE_USER_NOT_ACTIVE_EMAIL', $userRoles))
-          {
-            $url = $this->generateUrl('activation_request_page', ['login' => $user->getUsername()]);
-            $error = ['messageKey' => "Емейл не активирован, <a href='$url' style='display: block;font-family: IntroBookItalic, sans-serif;color: #FFF;text-decoration: underline;font-size: 14px;'>запросить ссылку активации</a>", 'messageData' => []];
-          }
-          $this->container->get('security.context')->setToken(null);
-        }
-      }
-      else
-      {
-        $log->error("not user");
-        $error = ['messageKey' => 'Логин или пароль не верны', 'messageData' => []];
-      }
-      
-      return JsonResponse::create(
-        [
-          'status'        => 400,
-          'last_username' => $lastUsername,
-          'error'         => $error,
-        ]);
     }
     
     /**
@@ -115,7 +45,6 @@
     public function logoutAction(Request $request)
     {
       $this->container->get('security.context')->setToken(null);
-      
       return $this->redirectToRoute('index_page');
     }
   }
--- src/AppBundle/Form/Type/Participant/ParticipantFieldsFormType.php
+++ src/AppBundle/Form/Type/Participant/ParticipantFieldsFormType.php
@@ -2,7 +2,6 @@
   
   namespace AppBundle\Form\Type\Participant;
   
-  use Doctrine\DBAL\ConnectionException;
   use Symfony\Component\OptionsResolver\OptionsResolver;
   use Symfony\Component\Form\AbstractType;
   use Symfony\Component\Form\Form;
@@ -13,17 +12,18 @@
   use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
   use Symfony\Component\Form\CallbackTransformer;
   use Dalee\PEPUWSClientBundle\Controller\GeoApiController;
-  use Symfony\Component\Security\Acl\Exception\Exception;
-
+  
   abstract class ParticipantFieldsFormType extends AbstractType
   {
+    protected $geoApi;
     protected $booleanToYNFormatCallbackTransformer;
+    
     public function configureOptions(OptionsResolver $resolver)
     {
       $resolver->setDefaults([
         'translation_domain' => 'personal'
       ]);
-    
+      
       $this->geoApi = new GeoApiController();
       $this->booleanToYNFormatCallbackTransformer = new CallbackTransformer(
         function ($value) {
@@ -35,29 +35,46 @@
       );
     }
     
-    public function callbackGeoFields(FormEvent $event)
-    {
+    public function callbackGeoFields(FormEvent $event) {
       $form = $event->getForm();
       $formData = $event->getData();
       
-      $options = ['empty_data' => ''];
-      if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType')
-      {
-        $options['disabled'] = true;
+      $countries = $this->geoApi->getCountries();
+      if (count($countries) > 0) {
+        if (count($countries) == 1) {
+          $options = ['empty_data'  => $countries[0]->getCode()];
+          if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType') {
+            $options['disabled'] = true;
+          }
+          $form->add('countrycode', HiddenType::class, $options);
+        } else {
+          $choices = [];
+          foreach ($countries as $country) {
+            $choices[$country->getTitle()] = $country->getCode();
+          }
+          $options = ['placeholder' => ' ', 'choices_as_values' => true, 'choices'  => $choices];
+          if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType') {
+            $options['disabled'] = true;
+          }
+          $form
+            ->add('countrycode', ChoiceType::class, $options);
+        }
       }
-      $form->add('countrycode', HiddenType::class, $options);
       
-      $countryCode = 'RU';
-      
+      $countryCode = null;
+      if (is_array($formData) && array_key_exists('countrycode', $formData) && $formData['countrycode']) {
+        $countryCode = $formData['countrycode'];
+      }
+      if (count($countries) == 1) {
+        $countryCode = $countries[0]->getCode();
+      }
       $regions = $this->addRegions($form, $countryCode);
       
       $regionGuid = null;
-      if (is_array($formData) && array_key_exists('regionguid', $formData))
-      {
+      if (is_array($formData) && array_key_exists('regionguid', $formData)) {
         $regionGuid = $formData['regionguid'];
       }
-      if (count($regions) == 1)
-      {
+      if (count($regions) == 1) {
         $regionGuid = $regions[0]->getGuid();
       }
       $this->addCities($form, $countryCode, $regionGuid);
@@ -66,77 +83,56 @@
     protected function addRegions(Form $form, $countryCode = null)
     {
       $regions = [];
-      if (!is_null($countryCode))
-      {
-        $regions = $this->getDoctrine()->getRepository('AppBundle:Region')->findAll();
+      if (!is_null($countryCode)) {
+        $regions = $this->geoApi->getRegionsByCountryCode($countryCode);
       }
-      if (count($regions) == 1)
-      {
-        $options = ['empty_data' => $regions[0]->getGuid()];
-        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType')
-        {
+      if (count($regions) == 1) {
+        $options = ['empty_data'  => $regions[0]->getGuid()];
+        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType') {
           $options['disabled'] = true;
         }
         $form->add('regionguid', HiddenType::class, $options);
-      }
-      else
-      {
+      } else {
         $choices = [];
-        foreach ($regions as $region)
-        {
-          
-          $title = $region->getName();
+        foreach ($regions as $region) {
+          $title = $region->getTitle();
           if ($region->getShortname()) {
-            $title = $region->getShortName() . ' ' . $title;
+            $title = $region->getShortname() . ' ' . $title;
           }
-  
           $choices[$title] = $region->getGuid();
         }
-        $options = ['placeholder' => ' ', 'choices_as_values' => true, 'choices' => $choices];
-        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType')
-        {
+        $options = ['placeholder' => ' ', 'choices_as_values' => true, 'choices'  => $choices];
+        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType') {
           $options['disabled'] = true;
         }
         $form->add('regionguid', ChoiceType::class, $options);
       }
-      
       return $regions;
     }
     
     protected function addCities(Form $form, $countryCode = null, $regionGuid = null)
     {
       $cities = [];
-      if (!is_null($countryCode) && !is_null($regionGuid))
-      {
-        $cities = $this->getDoctrine()->getRepository('AppBundle:City')->findByRegion($regionGuid);
+      if (!is_null($countryCode) && !is_null($regionGuid)) {
+        $cities = $this->geoApi->getCitiesByCountryCodeAndRegionGuid($countryCode, $regionGuid);
       }
-      if (count($cities) == 1)
-      {
-        $options = ['empty_data' => $cities[0]->getGuid()];
-        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType')
-        {
+      if (count($cities) == 1) {
+        $options = ['empty_data'  => $cities[0]->getGuid()];
+        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType') {
           $options['disabled'] = true;
         }
         $form->add('cityguid', HiddenType::class, $options);
-      }
-      else
-      {
+      } else {
         $choices = [];
-        foreach ($cities as $city)
-        {
-          $title = $city['name'];
-          if ($city['short_name']) {
-            if($city['short_name']!=='г')
-            {
-              $title = $city['short_name'] . '. ' . $title;
-            }
+        foreach ($cities as $city) {
+          $title = $city->getTitle();
+          if ($city->getShortname()) {
+            $title = $city->getShortname() . '. ' . $title;
           }
-  
-          $choices[$title] = $city['guid'];
+          $choices[$title] = $city->getGuid();
         }
-        $options = ['placeholder' => ' ', 'choices_as_values' => true, 'choices' => $choices];
-        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType')
-        {
+        $options = ['placeholder' => ' ', 'choices_as_values' => true, 'choices'  => $choices];
+        if (static::class == 'AppBundle\Form\Type\Participant\PersonalProfileFormType') {
           $options['disabled'] = true;
         }
         $form->add('cityguid', ChoiceType::class, $options);
