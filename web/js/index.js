a2a7bb9a9fb2d1a1a6637a211f174bb98d8ebf49
ram <perkes45@yandex.ru>
2018-02-27 15:00:39 -0500

    Update index.js

--- web/js/index.js
+++ web/js/index.js
@@ -55,8 +55,8 @@ $(function() {
         el.toggle();
         $('.faq-body').not(el).hide();
         //$(".js-faq-wrapper").scrollbar("resize");
-        if ($('.faq-title').length == $('.faq-title').index($(this))) {
-            $(".js-faq-wrapper").scrollbar("scroll", 1000000)
+        if (($('.faq-title').length -1 )== $('.faq-title').index($(this))) {
+            $(".js-faq-wrapper").scrollbar("scroll", 1000000);            
         }
     })
 
