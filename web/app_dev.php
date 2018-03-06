758e0d57ccbd32d3e543bfec5b90cf098408463e
WreWolf <wrewolf@gmail.com>
2018-03-06 18:00:28 +0300

    lock

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,7 +4,15 @@
   
   use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\Debug\Debug;
-
+  
+  if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '5.228.5.133'], true) || PHP_SAPI === 'cli-server')
+  ) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file.');
+  }
+  
   /** @var \Composer\Autoload\ClassLoader $loader */
   $loader = require __DIR__ . '/../app/autoload.php';
   Debug::enable();
